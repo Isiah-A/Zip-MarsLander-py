47cc7efd473134c14629271db09748ec189a497a
isiah-a <isiaha478@gmail.com>
2025-04-06 21:27:35 -0400

    Added computer to sim

--- marslander/Simulation.py
+++ marslander/Simulation.py
@@ -75,10 +75,11 @@ class Simulation:
     @staticmethod
     def main():
         # create a new BurnInputStream
-        burnSource = BurnInputStream()
+        # burnSource = BurnInputStream()
         # create a new Simulation object with a random starting altitude
         game = Simulation(Vehicle(Simulation.random_altitude()))
         # pass the new BurnInputStream to the run_simulation method
+        burnSource = OnBoardComputer()
         result = game.run_simulation(burnSource)
         return result
 
